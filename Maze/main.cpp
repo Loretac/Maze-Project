8e7e92d5e823c2cb2240066a5722073c03166fe2
Chris Loreta <LC@vt.edu>
2017-11-22 18:11:26 -0500

    Initial Commit

---
+++ Maze/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Maze
+//
+//  Created by Chris Loreta on 11/22/17.
+//  Copyright © 2017 Chris Loreta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
